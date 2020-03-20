a94c6d02c902dafcd421c847102341d1ae10b8f9
Joyce Leung <joisuryu@gmail.com>
2020-03-20 13:55:22 +1300

    contact updated

--- getStockCrisisDateWeb/setup.py
+++ getStockCrisisDateWeb/setup.py
@@ -6,11 +6,11 @@ setup(
     name='StockCrisisDateWeb',
     version='0.1',
     author='Joyce Leung',
-    author_email="joisuryu@gmail.com",
+    author_email="doublefin576@gmail.com",
     description="Web version for collecting date of stock crisis",
     license="GPLv3+",
     packages=["StockCrisisDateWeb"],
-    url="https://github.com/jo9553/Startup_option",
+    url="https://github.com/jo9553/prjStock",
     install_requires=[
         'pandas',
         'tables',  # Needed by pandas.HDFStorei
